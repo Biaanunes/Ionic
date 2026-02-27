6f4fcbdbe9bd1e02c67876c064ab05e78f0ff93c
Biaanunes <bianunesdeveras@gmail.com>
2026-02-27 08:26:45 -0300

    Create ex1ac1.adaptado.ts

---
+++ ex1ac1.adaptado.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common'; //
+import { FormsModule } from '@angular/forms'; //
+
+//todos os com comentarios foram adicionados para rodar no meu proprio computado em casa
+//Lembrete de tira-los na escola para rodar normal
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true, //era false
+  imports: [IonicModule, CommonModule, FormsModule], //
+})
+export class HomePage {
+  medida: string = '';
+  titulo: string = '';
+  valorAConverter: string = '';
+  conversao: string = '';
+  nome: string = '';
+
+  constructor() {}
+
+    converter() {
+
+      const valor = document.getElementById('opcao') as HTMLIonSegmentElement;
+
+      if(valor.value === 'celc'){
+        this.nome = 'celcius';
+        this.medida = 'ºC'
+        this.conversao = ((parseFloat(this.valorAConverter) - 32) * (5 / 9)).toFixed(0);
+
+      }else{
+        this.nome = 'fahrenheit';
+        this.medida = 'ºF'
+        this.conversao = ((9 * parseFloat(this.valorAConverter))+160 / 5).toFixed(0);
+    }
+
+      }
+    
+    }
